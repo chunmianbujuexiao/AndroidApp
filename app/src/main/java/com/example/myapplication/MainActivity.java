aad181845dfaba745b7b30c8a01dbb1afd7df8c7
chenpeng <3144652865@qq.com>
2017-04-13 22:08:44 +0800

    test branch

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -100,7 +100,7 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+
         int id = item.getItemId();
         onNavigationItemSelectedExecute(id);
 
